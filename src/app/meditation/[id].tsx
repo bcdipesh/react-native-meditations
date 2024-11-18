93b87d44becfb16e7605ba3b0aae94c57c906616
Dipesh B C <bcdipeshwork@gmail.com>
2024-11-18 16:19:46 -0500

    Added audio player using new expo-audio library

--- src/app/meditation/[id].tsx
+++ src/app/meditation/[id].tsx
@@ -6,12 +6,23 @@ import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 import Slider from "@react-native-community/slider";
+import { useAudioPlayer, useAudioPlayerStatus } from "expo-audio";
 
 import { meditations } from "@/data";
+import audio from "@assets/meditations/audio1.mp3";
 
 export default function MeditationDetails() {
     const { id } = useLocalSearchParams<{ id: string }>();
     const meditation = meditations.find((m) => m.id === Number(id));
+    const player = useAudioPlayer(audio);
+    const status = useAudioPlayerStatus(player);
+
+    const formatSeconds = (milliseconds: number) => {
+        const minutes = Math.floor(milliseconds / 60000);
+        const seconds = Math.floor((milliseconds % 60000) / 1000);
+
+        return `${minutes}:${seconds.toString().padStart(2, "0")}`;
+    };
 
     if (!meditation) {
         return <Text>Meditation not found!</Text>;
@@ -39,10 +50,18 @@ export default function MeditationDetails() {
                         {meditation?.title}
                     </Text>
                 </View>
-                <View className="bg-zinc-800 self-center w-24 aspect-square rounded-full items-center justify-center">
-                    <FontAwesome6 name="play" size={24} color="snow" />
-                    {/* <FontAwesome6 name="pause" size={24} color="black" /> */}
-                </View>
+                <Pressable
+                    onPress={() =>
+                        player.playing ? player.pause() : player.play()
+                    }
+                    className="bg-zinc-800 self-center w-24 aspect-square rounded-full items-center justify-center"
+                >
+                    <FontAwesome6
+                        name={status.playing ? "pause" : "play"}
+                        size={24}
+                        color="snow"
+                    />
+                </Pressable>
                 <View className="flex-1">
                     <View className="p-10 mt-auto gap-5">
                         <View className="flex-row justify-between">
@@ -60,9 +79,9 @@ export default function MeditationDetails() {
                         <View>
                             <Slider
                                 style={{ width: "100%", height: 3 }}
-                                value={0.5}
+                                value={status.currentTime / status.duration}
                                 onSlidingComplete={(value) =>
-                                    console.log(value)
+                                    player.seekTo(value * status.duration)
                                 }
                                 minimumValue={0}
                                 maximumValue={1}
@@ -72,8 +91,8 @@ export default function MeditationDetails() {
                             />
                         </View>
                         <View className="flex-row justify-between">
-                            <Text>03:57</Text>
-                            <Text>11:27</Text>
+                            <Text>{formatSeconds(status.currentTime)}</Text>
+                            <Text>{formatSeconds(status.duration)}</Text>
                         </View>
                     </View>
                 </View>
