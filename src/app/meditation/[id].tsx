3e44b519db446498c7ba3d80e589e4f72087a97b
Dipesh B C <bcdipeshwork@gmail.com>
2024-11-18 16:00:53 -0500

    Complete meditation details screen ui

--- src/app/meditation/[id].tsx
+++ src/app/meditation/[id].tsx
@@ -2,7 +2,10 @@ import { Text, View, Pressable } from "react-native";
 import { router, useLocalSearchParams } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import AntDesign from "@expo/vector-icons/AntDesign";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import Slider from "@react-native-community/slider";
 
 import { meditations } from "@/data";
 
@@ -15,37 +18,64 @@ export default function MeditationDetails() {
     }
 
     return (
-        <SafeAreaView className="bg-orange-500 flex-1">
-            <View className="flex-row items-center justify-between p-10">
-                <AntDesign name="infocirlceo" size={24} color="black" />
-
-                <View className="bg-zinc-800 p-2 rounded-md">
-                    <Text className="text-zinc-100 font-semibold">
-                        Today's meditation
+        <SafeAreaView className="bg-orange-500 flex-1 justify-between">
+            <View className="flex-1">
+                <View className="flex-1">
+                    <View className="flex-row items-center justify-between p-10">
+                        <AntDesign name="infocirlceo" size={24} color="black" />
+                        <View className="bg-zinc-800 p-2 rounded-md">
+                            <Text className="text-zinc-100 font-semibold">
+                                Today's meditation
+                            </Text>
+                        </View>
+                        <AntDesign
+                            onPress={() => router.back()}
+                            name="close"
+                            size={24}
+                            color="black"
+                        />
+                    </View>
+                    <Text className="text-3xl text-center font-semibold px-2">
+                        {meditation?.title}
                     </Text>
                 </View>
-
-                <AntDesign
-                    onPress={() => router.back()}
-                    name="close"
-                    size={24}
-                    color="black"
-                />
-            </View>
-
-            <Text className="text-3xl text-center font-semibold px-2">
-                {meditation?.title}
-            </Text>
-
-            <View className="bg-zinc-800 self-center w-24 aspect-square rounded-full items-center justify-center">
-                <FontAwesome6 name="play" size={24} color="snow" />
-                {/* <FontAwesome6 name="pause" size={24} color="black" /> */}
-            </View>
-
-            <View className="p-10">
-                <View className="flex-row justify-between">
-                    <FontAwesome6 name="play" size={24} color="#3a3937" />
-                    <AntDesign name="setting" size={24} color="#3a3937" />
+                <View className="bg-zinc-800 self-center w-24 aspect-square rounded-full items-center justify-center">
+                    <FontAwesome6 name="play" size={24} color="snow" />
+                    {/* <FontAwesome6 name="pause" size={24} color="black" /> */}
+                </View>
+                <View className="flex-1">
+                    <View className="p-10 mt-auto gap-5">
+                        <View className="flex-row justify-between">
+                            <MaterialIcons
+                                name="airplay"
+                                size={24}
+                                color="#3a3937"
+                            />
+                            <MaterialCommunityIcons
+                                name="cog-outline"
+                                size={24}
+                                color="#3a3937"
+                            />
+                        </View>
+                        <View>
+                            <Slider
+                                style={{ width: "100%", height: 3 }}
+                                value={0.5}
+                                onSlidingComplete={(value) =>
+                                    console.log(value)
+                                }
+                                minimumValue={0}
+                                maximumValue={1}
+                                minimumTrackTintColor="#3a3937"
+                                maximumTrackTintColor="#3a393755"
+                                thumbTintColor="#3a3937"
+                            />
+                        </View>
+                        <View className="flex-row justify-between">
+                            <Text>03:57</Text>
+                            <Text>11:27</Text>
+                        </View>
+                    </View>
                 </View>
             </View>
         </SafeAreaView>
